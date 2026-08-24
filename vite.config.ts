9d9a2b1353576166f546f6fd89e2f062cb7148ad
Caua22908 <caua.ramalho2014@gmail.com>
2026-08-24 15:29:54 -0300

    preparando projeto2

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,8 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   base: "/sweet-surprises-landing/",
   tanstackStart: {
-    server: { entry: "server" },
-  },
-  nitro: {
-    preset: "static",
-    prerender: {
-      routes: ["/sweet-surprises-landing/", "/"],
-      crawlLinks: true,
-      failOnError: false,
+    spa: {
+      enabled: true,
     },
   },
 } as any);
