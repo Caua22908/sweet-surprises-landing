80c05ac0616a3282f8a0af27f9670fdec4b68096
Caua22908 <caua.ramalho2014@gmail.com>
2026-08-24 16:58:40 -0300

    fix: adiciona hash history para github pages1

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,11 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   base: "/sweet-surprises-landing/",
   tanstackStart: {
+    prerender: {
+      enabled: false,
+    },
     spa: {
-      enabled: true,
+      enabled: false,
     },
   },
 } as any);
